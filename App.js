631d1bc782e4e90cfb79433b2929825c92a92185
Your Name <royalguptayes@gmail.com>
2022-08-02 12:16:08 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState,useEffect } from 'react';
+import { StyleSheet, Text, View , ActivityIndicator} from 'react-native';
+import SearchBar from './components/SearchBar';
+import Weather from './components/Weather';
+const API_KEY ="xxxxplease fill api key";
+//
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(true);
+
+    async function fetchWeatherData(cityName) {
+        setLoaded(false);
+        const API =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+          try {
+            const response = await fetch(API);
+            if(response.status == 200) {
+                const data = await response.json();
+                setWeatherData(data);
+            } else {
+                setWeatherData(null);
+            }
+            setLoaded(true);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchWeatherData('pune');
+        console.log(weatherData);
+
+    }, [])
+    
+    console.log(weatherData);
+
+    if(!loaded) {
+      return (
+          <View style={styles.container}>
+              <ActivityIndicator color='red'  size={46} />
+          </View>
+
+      )
+  } else if(weatherData === null){
+    return(
+    <View>
+       <View style={styles.container}>
+                <SearchBar fetchWeatherData={fetchWeatherData}/>
+                <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+            </View>
+    </View>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData} />
+
     </View>
   );
 }
@@ -17,4 +67,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
 });
---
+++ assets/backGroundimage/index.js
@@ -0,0 +1,6 @@
+const haze = 'https://i.pinimg.com/originals/da/d6/5c/dad65c030621548b4f6d9dccbf3b12df.gif'
+const rainy = 'https://phoneky.co.uk/thumbs/screensavers/down/nature/bestrainvi_W1TQb4v3.gif'
+const snow = 'https://cutewallpaper.org/25/animated-winter-wallpaper-gif/free-download-winter-wallpaper-and-screensaver-pack-from-bing-hd-wallpapers-480x800-for-your-desktop-mobile-amp-tablet-explore-47-gif-wallpaper-for-windows-8-free-animated-wallpaper-windows-8-windows-81-gif-wallpaper.gif'
+const sunny = 'https://i.pinimg.com/originals/60/9e/44/609e44257fd33af41f9c6bc1e458838e.gif'
+
+export { haze, rainy, snow, sunny };
